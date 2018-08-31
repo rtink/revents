e486dc1459de381d12bd84688a39d59202008db3
Rhonda Tinkham <rtinkham@live.com>
2018-08-31 17:32:47 -0400

    Added routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 const rootEl = document.getElementById('root')
 
 let render = () => {
- ReactDOM.render(<App />, rootEl)   
+ ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+     , rootEl)   
 }
 
 if (module.hot) {
