cdf30f7086dfcf100022a72c18fc12d482ea0438
Rhonda Tinkham <rtinkham@live.com>
2018-09-08 23:14:22 -0400

    redux thunk and async

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,18 +1,33 @@
 import { createReducer } from '../../app/common/util/reducerUtil';
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testConstants';
+import { 
+    INCREMENT_COUNTER, 
+    DECREMENT_COUNTER,
+    COUNTER_ACTION_STARTED,
+    COUNTER_ACTION_FINISHED
+    } 
+    from './testConstants';
 
 
 const initialState = {
-    data: 34
-}
+    data: 34,
+    loading: false
+};
 
 export const incrementCounter = (state, payload) => {
-    return {...state, data: state.data + 1};
-}
+    return { ...state, data: state.data + 1} ;
+};
 
 export const decrementCounter = (state, payload) => {
-    return {...state, data: state.data - 1}
-}
+    return { ...state, data: state.data - 1 }
+};
+
+export const counterActionStarted = (state, payload) => {
+    return { ...state, loading: true }
+};
+
+export const counterActionFinished = (state, payload) => {
+    return { ...state, loading: false }
+};
 
 // const testReducer = (state = initialState, action) => {
 //     switch (action.type) {
@@ -27,5 +42,7 @@ export const decrementCounter = (state, payload) => {
 
 export default createReducer(initialState, {
     [INCREMENT_COUNTER]: incrementCounter,
-    [DECREMENT_COUNTER]: decrementCounter
-})
+    [DECREMENT_COUNTER]: decrementCounter,
+    [COUNTER_ACTION_STARTED]: counterActionStarted,
+    [COUNTER_ACTION_FINISHED]: counterActionFinished
+});
